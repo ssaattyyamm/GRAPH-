cb1c7ffdad0515a8ef63492b73a0ee17fa413cbf
noobcoderhuuu <satyam.singh.931559@gmail.com>
2026-06-07 11:20:16 +0530

    Time: 10 ms (50.00%) | Memory: 128.2 MB (50.00%) - LeetSync

--- 4142-maximum-total-value-of-covered-indices/maximum-total-value-of-covered-indices.java
+++ 4142-maximum-total-value-of-covered-indices/maximum-total-value-of-covered-indices.java
@@ -1,10 +1,21 @@
 class Solution {
     public long maxTotal(int[] nums, String s) {
-        //kya kr sakte hai :
 
-        //"011110" :  agar kabhi bhi consecutive one milte hai [start - 1 , end]  jaisa given tc mai consecutive one start hua first idx pr , end hua 4 pr  , then res = sum of all element from start -1 to end - Math.min(start-1 to end)
+        // Key Observation:
 
-        // jaisa hi zero hit hoga we subtract the min and update the minimum
+        // Example: "011110"
+        //
+        // Whenever we find a consecutive block of 1s,
+        // suppose the block starts at 'start' and ends at 'end'.
+        //
+        // Then we can think of the contribution as:
+        // sum of all elements in the range [start - 1, end]
+        // minus the minimum element from that range.
+        //
+        // Intuition:
+        // Is entire segment me hum sab values le sakte hain
+        // except one value, so obviously minimum value ko
+        // remove karna is the best choice.
 
         int n = nums.length;
 
@@ -13,24 +24,42 @@ class Solution {
         long min = Integer.MAX_VALUE;
 
         for (int i = n - 1; i >= 0; i--) {
+
             if (s.charAt(i) == '1') {
-                //we can add this :
+
+                // Current value can definitely contribute
+                // to the answer, so add it.
+
                 res += nums[i];
 
+                // Track minimum value of the current segment
                 min = Math.min(min, nums[i]);
 
             } else {
-                // agar kabhi bhi zero hit hota hai 
+
+                // Agar current 0 ke just right side par
+                // consecutive block of 1s start ho raha hai,
+                // then this 0 also becomes part of the segment.
+
                 if (i + 1 < n && s.charAt(i + 1) == '1') {
+
                     res += nums[i];
 
                     min = Math.min(min, nums[i]);
 
+                    // Finalize this segment:
+                    // remove the minimum element once.
+
                     res -= min;
                 }
+
+                // Segment khatam ho gaya,
+                // reset minimum for the next segment.
+
                 min = Integer.MAX_VALUE;
             }
         }
+
         return res;
     }
 }
