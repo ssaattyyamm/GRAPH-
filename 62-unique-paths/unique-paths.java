e2f2fe237410a0c740a372b73d1b5a343ad123f9
noobcoderhuuu <satyam.singh.931559@gmail.com>
2026-06-18 19:21:38 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (65.01%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,19 +1,25 @@
 class Solution {
+    int[][] dp;
     public int uniquePaths(int m, int n) {
-        int dp[][]  = new int[m][n];
+        dp = new int[m][n];
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i == 0 || j==0) dp[i][j] =1;
-            }
+        for(int[] d : dp){
+            Arrays.fill(d , -1);
         }
 
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
+        return helper(0 , 0 , m , n);
+    }
+    public int helper(int row , int col , int m , int n){
+        if(row == m-1 && col == n-1) return 1;
 
-        return dp[m-1][n-1];
+        if(row < 0 || row >= m || col < 0 || col >= n) return 0;
+
+        if(dp[row][col] != -1 ) return dp[row][col];
+
+        int right = helper(row, col +1 , m ,n);
+
+        int down = helper(row +1 , col , m , n);
+
+        return dp[row][col] =  right + down;
     }
 }
