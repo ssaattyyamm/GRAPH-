ac76dc004a3ff5f7198e83c99309872911a469bc
noobcoderhuuu <satyam.singh.931559@gmail.com>
2026-06-19 12:03:59 +0530

    Time: 2 ms (43.67%) | Memory: 47 MB (86.98%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,39 +1,65 @@
-class Solution {
-    public int findCircleNum(int[][] mat) {
-        int n = mat.length;
+class DSU{
+    int parent[];
+    int rank[];
 
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+    public DSU(int n){
+        parent = new int[n];
+        rank = new int[n];
 
         for(int i=0;i<n;i++){
-            ArrayList<Integer> list = new ArrayList<>();
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+    public int find(int u){
+        if(parent[u] == u) return u;
+
+        return parent[u] = find(parent[u]);
+    }
+
+    public void union(int u , int v){
+        int p1 = find(u);
+        int p2 = find(v);
+
+        int r1 = rank[p1];
+        int r2 = rank[p2];
+
+        if(p1 == p2) return;
+
+        if(r1 > r2){
+            parent[p2] = p1;
+        }
+        else if(r1 < r2){
+            parent[p1] = p2;
+        }
+        else{
+            parent[p1] = p2;
+            rank[p2]++;
+        }
+    }
+}
+class Solution {
+    public int findCircleNum(int[][] arr) {
+        int n = arr.length;
+
+        DSU dsu = new DSU(n);
+
+        for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(mat[i][j] == 1){
-                    list.add(j);
+                if(arr[i][j] == 1){
+                    dsu.union(i , j);
                 }
             }
-            adj.add(list);
         }
+        int res =0;
 
-
-        boolean visit[] = new boolean[n];
-
-        int res = 0;
 
         for(int i=0;i<n;i++){
-            if(!visit[i]){
-                dfs(adj, visit, i);
+            if(dsu.parent[i] == i){
                 res++;
             }
         }
-        return res;
-    }
-    public void dfs(ArrayList<ArrayList<Integer>> adj , boolean visit[], int curr){
-        visit[curr] = true;
 
-        for(int x : adj.get(curr)){
-            if(visit[x] == false){
-                dfs(adj,visit,x);
-            }
-        }
+        return res;
     }
 }
