87ff1dc1571e813e7a2db94226f9adcfcc60b764
noobcoderhuuu <satyam.singh.931559@gmail.com>
2026-06-20 11:30:46 +0530

    Time: 4 ms (45.21%) | Memory: 51.8 MB (79.87%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,53 +1,40 @@
-class Pair{
-    int first;
-    int second;
-    
-    public Pair(int first , int second){
-        this.first = first;
-        this.second = second;
-    }
-}
 class Solution {
-    public int numIslands(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+    int visit[][];
 
-        int dir[][] = {{-1,0},{0,1},{1,0},{0,-1}};
+    public int numIslands(char[][] arr) {
+        int n =  arr.length;
 
-        int[][] visit = new int[n][m];
+        int m = arr[0].length;
 
-        int res = 0;
+        visit= new int[n][m];
+
+        int row[]={-1 , 0 ,0 ,1};
+
+        int col[]={0, -1,1,0};
+
+        int cnt = 0;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == '1' && visit[i][j] == 0){
-                    res++;
-                    bfs(i ,j , grid, visit, dir , n , m);
+                if(visit[i][j] != 1 && arr[i][j] == '1'){
+                    cnt++;
+                    dfs(arr , i , j , row , col);
                 }
             }
         }
-        return res;
+
+        return cnt;
     }
-    public void bfs(int  row , int col , char[][] grid , int[][] visit , int[][] dir , int n , int m ){
-        visit[row][col] = 1;
 
-        Queue<Pair> q = new LinkedList<>();
+    public void dfs(char[][] arr, int r, int c, int[] row, int[] col) {
+        visit[r][c] = 1;
 
-        q.add(new Pair(row , col));
+        for (int i = 0; i < 4; i++) {
+            int nr = r + row[i];
+            int nc = c + col[i];
 
-        while(q.size() > 0){
-            int r = q.peek().first;
-            int c = q.peek().second;
-            q.remove();
-
-            for(int d[] : dir){
-                int nr = r + d[0];
-                int nc = c + d[1];
-
-                if(nr >=0 && nr < n && nc >= 0 && nc < m && visit[nr][nc] == 0 && grid[nr][nc] == '1'){
-                    visit[nr][nc] = 1;
-                    q.add(new Pair(nr , nc));
-                }
+            if(nr >= 0 && nr < arr.length  && nc >= 0 && nc < arr[0].length && visit[nr][nc] != 1 && arr[nr][nc] == '1'){
+                dfs(arr ,nr , nc ,row , col);
             }
         }
     }
