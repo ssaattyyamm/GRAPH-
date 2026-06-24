99b12a72087803b8e1e616eec90d9e83dd6353d1
noobcoderhuuu <satyam.singh.931559@gmail.com>
2026-06-24 12:14:21 +0530

    Time: 26 ms (38.67%) | Memory: 62 MB (54.74%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,58 +1,52 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        //thought process :  simple hoga 
-        // mai kya kehrha hu jo bhi nodes hai unhe revese krdenga 
-        // terminal node : indeg = 0;
-        //un nodes ko queue k andar dal dunga 
+        int n = graph.length;
 
-        // we can use the concept of topological sort
+        ArrayList<ArrayList<Integer>>  adj = new ArrayList<>();
 
-        int v = graph.length;
+        int indeg[] = new int[n];
 
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
-        for(int i=0;i<graph.length;i++){
+        for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
 
-       //adj list create 
-
-       for(int i=0;i<v;i++){
-        for(int j=0;j<graph[i].length;j++){
-            adj.get(graph[i][j]).add(i);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<graph[i].length;j++){
+                adj.get(graph[i][j]).add(i);
+                indeg[i]++;
+            }
         }
-       }
 
-       int indeg[] = new int[v];
+        Queue<Integer> queue = new LinkedList<>();
 
-       for(int i=0;i<v;i++){
-        for(int x : adj.get(i)){
-            indeg[x]++;
+        for(int i=0;i<n;i++){
+            if(indeg[i] == 0){
+                queue.add(i);
+            }
         }
-       }
 
-       Queue<Integer> q = new LinkedList<>();
+        ArrayList<Integer> res = new ArrayList<>();
 
-       for(int i=0;i<v;i++){
-        if(indeg[i] == 0) q.add(i);
-       }
+        while(queue.size() > 0){
+            int node = queue.peek();
 
-       ArrayList<Integer> res = new ArrayList<>();
+            queue.poll();
 
-       while(q.size() > 0){
-        int curr = q.peek();
-        res.add(curr);
-        q.poll();
+            res.add(node);
 
-        for(int x : adj.get(curr)){
-            indeg[x]--;
+            for(int i=0;i<adj.get(node).size();i++){
+                int curr = adj.get(node).get(i);
 
-            if(indeg[x] == 0) q.add(x);
+                indeg[curr]--;
+
+                if(indeg[curr] == 0){
+                    queue.add(curr);
+                }
+            }
         }
-       }
 
-       Collections.sort(res);
+        Collections.sort(res);
 
-       return res;
+        return res;
     }
 }
