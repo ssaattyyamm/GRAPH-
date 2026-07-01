06478efc55b7d990bc809ecee1e661e8169e98bb
noobcoderhuuu <satyam.singh.931559@gmail.com>
2026-07-01 15:37:39 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (18.40%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,23 +1,25 @@
 class Solution {
-    int[] dp;
     public int rob(int[] nums) {
+        // this is the very simple problem 
         int n = nums.length;
 
-        dp = new int[n];
+        if(n == 1) return nums[0];
 
-        Arrays.fill(dp , -1);
+        int dp[] = new int[n];
 
-        return helper(nums , 0);
+        dp[0] = nums[0];
+
+        dp[1] = Math.max(nums[0] , nums[1]);
+
+        for(int i=2;i<n;i++){
+            dp[i] = Math.max(nums[i] + dp[i-2] , dp[i-1]);
+        }
+
+        return dp[n-1];
     }
-    public int helper(int[] nums , int idx ){
-        if(idx >= nums.length) return 0;
+    public int helper(int idx  , int[] nums ){
+        if(idx  >= nums.length) return 0;
 
-        if(dp[idx] != -1) return dp[idx];
-
-        int pick = nums[idx] + helper(nums , idx +2);
-
-        int skip = helper(nums , idx +1);
-
-        return dp[idx] =  Math.max(pick , skip);
+        return Math.max(nums[idx] + helper(idx + 2 , nums) , helper(idx + 1 , nums));
     }
 }
