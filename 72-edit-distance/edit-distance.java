2ced677f81cf10e964d9767cdc4297fcb1895488
noobcoderhuuu <satyam.singh.931559@gmail.com>
2026-07-02 22:24:22 +0530

    Time: 5 ms (69.12%) | Memory: 47.1 MB (52.99%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,40 +1,29 @@
 class Solution {
-    int n, m;
-    int dp[][];
     public int minDistance(String s, String t) {
-        n = s.length();
-        m = t.length();
+        int n =s.length();
+        int m = t.length();
 
-        dp = new int[n+1][m+1];
+        int dp[][] = new int[n+1][m+1];
 
         for(int i=0;i<=n;i++){
-            Arrays.fill(dp[i] , -1);
-        }
-        return helper(s, t, n, m);
-    }
-
-    public int helper(String s, String t, int i, int j) {
-        if (i != 0 && j == 0)
-            return i;
-
-        if (i == 0 && j != 0)
-            return j;
-
-        if (i == 0 && j == 0)
-            return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if (s.charAt(i - 1) == t.charAt(j - 1)) {
-            return dp[i][j] =  helper(s, t, i - 1, j - 1);
-
-        } else {
-            int insert = 1 + helper(s, t, i, j - 1);
-            int remove = 1 + helper(s, t, i - 1, j);
-            int replace = 1 + helper(s, t, i - 1, j - 1);
-
-            return dp[i][j] = Math.min(insert, Math.min(remove, replace));
+            dp[i][0] = i;
         }
 
+        for(int j=0;j<=m;j++){
+            dp[0][j] = j;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 1 +  Math.min(dp[i-1][j-1] , Math.min(dp[i-1][j]  , dp[i][j-1]));
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 }
