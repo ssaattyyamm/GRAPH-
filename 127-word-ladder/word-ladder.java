ef96bbe951e31dbcfb8acd8603ae473bad7500fd
noobcoderhuuu <satyam.singh.931559@gmail.com>
2026-07-02 13:30:54 +0530

    Time: 74 ms (61.21%) | Memory: 48.8 MB (31.41%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,7 +1,7 @@
 class Pair{
     String word;
     int steps;
-
+    
     public Pair(String word , int steps){
         this.word = word;
         this.steps = steps;
@@ -9,39 +9,36 @@ class Pair{
 }
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        HashSet<String> set = new HashSet<>();
-
-        for(String w : wordList){
-            set.add(w);
-        }
-
-        // set 0(1)
-
+        ///target 5 min 
         Queue<Pair> queue = new LinkedList<>();
-        
+
         queue.add(new Pair(beginWord , 1));
 
+        Set<String> set = new HashSet<>();
+
+        for(int i=0;i<wordList.size();i++){
+            set.add(wordList.get(i));
+        }
+
         while(queue.size() > 0){
-            String w = queue.peek().word;
+            String curr = queue.peek().word;
             int step = queue.peek().steps;
 
-            if(endWord.equals(w)) return step;
- 
+            if(endWord.equals(curr)) return step;
+
             queue.poll();
 
-            char arr[] = w.toCharArray();
+            char arr[] = curr.toCharArray();
 
             for(int i=0;i<arr.length;i++){
                 char original = arr[i];
-
-                for(char ch = 'a' ; ch<='z' ;ch++){
+                for(char ch = 'a' ; ch <='z';ch++){
                     arr[i] = ch;
+                    String s = new String(arr);
 
-                    String curr = new String(arr);
-
-                    if(set.contains(curr)){
-                        queue.add(new Pair(curr ,step +1));
-                        set.remove(curr);
+                    if(set.contains(s)){
+                        queue.add(new Pair(s , step + 1));
+                        set.remove(s);
                     }
                 }
                 arr[i] = original;
