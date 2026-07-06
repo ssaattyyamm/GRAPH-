b4ca159df73598c55542c26cb545cf5cc90e83fa
noobcoderhuuu <satyam.singh.931559@gmail.com>
2026-07-06 12:00:22 +0530

    Time: 1 ms (72.47%) | Memory: 42.9 MB (91.93%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -1,30 +1,38 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
         int n = days.length;
-        int[] dp = new int[n + 1];
 
-        for (int i = n - 1; i >= 0; i--) {
-            int w1 = costs[0] + dp[lb(days, i + 1, n - 1, days[i] + 1)];
-            int w2 = costs[1] + dp[lb(days, i + 1, n - 1, days[i] + 7)];
-            int w3 = costs[2] + dp[lb(days, i + 1, n - 1, days[i] + 30)];
+        int dp[] = new int[n+1];
 
-            dp[i] = Math.min(w1, Math.min(w2, w3));
+        dp[n] = 0;
+        // dp[n-1] = Math.min(costs[0] , Math.min(costs[1] , costs[2]));
+
+        for(int i=n-1;i>=0;i--){
+            // day 1
+
+            int w1 = costs[0] + dp[lb(days , i + 1 , n-1  , days[i] + 1)];
+
+            int w2 = costs[1] + dp[lb(days , i + 1 , n-1  , days[i] + 7)];
+
+            int w3 = costs[2] + dp[lb(days , i + 1 , n-1  , days[i] + 30)];
+
+            dp[i] = Math.min(w1 , Math.min(w2 , w3));
         }
-
         return dp[0];
     }
-
-    private int lb(int[] arr, int start, int end, int target) {
+    public int lb(int[] arr , int start , int end , int target){
         int ans = arr.length;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+        while(start <= end){
+            int mid = start + (end - start)/2;
 
-            if (arr[mid] >= target) {
+            if(arr[mid] >= target){
                 ans = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+                end = mid-1;
+            }
+
+            else{
+                start = mid +1;
             }
         }
 
