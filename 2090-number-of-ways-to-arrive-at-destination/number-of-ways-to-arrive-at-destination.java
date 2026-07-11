bf7af93a9603a15c1a04c81b53fc4436ba188884
noobcoderhuuu <satyam.singh.931559@gmail.com>
2026-07-12 02:40:44 +0530

    Time: 15 ms (25.51%) | Memory: 52.5 MB (5.96%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -1,70 +1,59 @@
-import java.util.*;
-
-class Pair {
-    int value;
+class Pair{
+    int node;
     long cost;
 
-    public Pair(int value, long cost) {
-        this.value = value;
+    public Pair(int node , long cost){
+        this.node = node;
         this.cost = cost;
     }
 }
-
 class Solution {
     public int countPaths(int n, int[][] roads) {
-
-        long MOD = 1000000007L;
-
+        int MOD = 1000000007;
+        int inf = (int) 1e9;
         ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0;i<n;i++){
             adj.add(new ArrayList<>());
         }
 
-        for (int[] r : roads) {
-            adj.get(r[0]).add(new Pair(r[1], r[2]));
-            adj.get(r[1]).add(new Pair(r[0], r[2]));
+        for(int r[] : roads){
+            adj.get(r[0]).add(new Pair(r[1] , (long)r[2]));
+            adj.get(r[1]).add(new Pair(r[0] ,(long)r[2]));
         }
 
-        PriorityQueue<Pair> pq =
-                new PriorityQueue<>((a, b) -> Long.compare(a.cost, b.cost));
+        long  visit[] = new long[n];
+        long ways[] = new long[n];
 
-        long[] dist = new long[n];
-        Arrays.fill(dist, Long.MAX_VALUE);
-
-        long[] ways = new long[n];
-
-        dist[0] = 0;
         ways[0] = 1;
 
-        pq.add(new Pair(0, 0));
+        Arrays.fill(visit , Long.MAX_VALUE);
 
-        while (!pq.isEmpty()) {
+        visit[0] = 0;
 
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> Long.compare(a.cost , b.cost));
+
+        pq.add(new Pair(0 , 0));
+
+        while(pq.size() > 0){
             Pair p = pq.poll();
-
-            int node = p.value;
+            int node = p.node;
             long cost = p.cost;
 
-            if (cost > dist[node]) continue;
+            for(int i = 0 ; i<adj.get(node).size();i++){
+                int curr = adj.get(node).get(i).node;
+                long wt   = adj.get(node).get(i).cost;
 
-            for (int i = 0; i < adj.get(node).size(); i++) {
-
-                int curr = adj.get(node).get(i).value;
-                long c = adj.get(node).get(i).cost;
-
-                if (cost + c == dist[curr]) {
-                    ways[curr] = (ways[curr] + ways[node]) % MOD;
-                }
-                else if (cost + c < dist[curr]) {
-                    dist[curr] = cost + c;
+                if(visit[curr] > cost + wt){
+                    visit[curr] = cost + wt;
+                    pq.add(new Pair(curr , visit[curr]));
                     ways[curr] = ways[node];
-
-                    pq.add(new Pair(curr, dist[curr]));
+                }
+                else if(visit[curr] == cost + wt){
+                    ways[curr] = (ways[curr] + ways[node]) % MOD;
                 }
             }
         }
-
-        return (int) (ways[n - 1] % MOD);
+        return (int) (ways[n-1] % MOD);
     }
 }
