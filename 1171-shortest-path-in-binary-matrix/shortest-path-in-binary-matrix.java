7da63678473c677c8622edf10425f6f34dd26dc8
noobcoderhuuu <satyam.singh.931559@gmail.com>
2026-07-11 21:33:24 +0530

    Time: 16 ms (57.12%) | Memory: 47.3 MB (85.82%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,60 +1,55 @@
-class Tuple {
-    int dist;
+class Tuple{
     int row;
     int col;
+    int wt;
 
-    public Tuple(int dist, int row, int col) {
-        this.dist = dist;
+    public Tuple(int row , int col , int wt){
         this.row = row;
         this.col = col;
+        this.wt = wt;
     }
 }
-
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int inf = (int)1e9;
-
+        int inf = (int) 1e9;
         int n = grid.length;
+        int m = grid[0].length;
 
-        int res[][] = new int[n][n];
+        if(grid[0][0] == 1 || grid[n-1][m-1] == 1) return -1;
 
-        if(grid[0][0] == 1) return -1;
+        int row[] = {-1 , -1 ,-1 ,0  , 0 , 1 , 1, 1};
+        int col[] = {-1 , 0 , 1 , -1  ,1 ,-1 ,0 ,1};
 
-        for (int r[] : res) {
-            Arrays.fill(r, inf);
+        int visit[][] = new int[n][m];
+
+        for(int v[] : visit){
+            Arrays.fill(v , inf);
         }
 
-        res[0][0] = 0;
+        visit[0][0] = 1;
 
         Queue<Tuple> queue = new LinkedList<>();
 
-        queue.add(new Tuple(0, 0, 0));
-
-        int row[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
-        int col[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
-
-        // how can i do this : 
+        queue.add(new Tuple(0 , 0 , 1));
 
         while(queue.size() > 0){
-            int dist = queue.peek().dist;
-            int r = queue.peek().row;
-            int c = queue.peek().col;
+            Tuple t = queue.poll();
 
-            queue.remove();
+            int r = t.row;
+            int c = t.col;
+            int wt = t.wt;
 
             for(int i=0;i<8;i++){
                 int nr = r + row[i];
                 int nc = c + col[i];
-
-                if(nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] != 1){
-                    if(dist + 1 < res[nr][nc]){
-                        res[nr][nc] = dist +1;
-                        queue.add(new Tuple(dist + 1 , nr , nc));
-                    }
+                
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && visit[nr][nc] > 1 + wt && grid[nr][nc] == 0){
+                    visit[nr][nc] = 1 + wt;
+                    queue.add(new Tuple(nr , nc , visit[nr][nc]));
                 }
             }
         }
 
-        return res[n-1][n-1] == inf ? -1 : res[n-1][n-1] +1;
+        return visit[n-1][m-1] == inf ? -1 : visit[n-1][m-1];
     }
 }
