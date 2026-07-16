77a09d23a7293fd2dfd2bbd2b84da080dd1aa3c2
noobcoderhuuu <satyam.singh.931559@gmail.com>
2026-07-16 08:50:38 +0530

    Time: 9 ms (51.19%) | Memory: 58.9 MB (63.15%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,63 +1,54 @@
 class Pair{
     int row;
     int col;
-
-    public Pair(int row , int col){
-        this.row = row;
-        this.col = col;
-    }
 }
 class Solution {
-    public int numEnclaves(int[][] arr) {
-        int n = arr.length;
-        int m = arr[0].length;
+    int row[];
+    int col[];
+    int n , m;
+    public int numEnclaves(int[][] grid) {
+         n = grid.length;
+         m = grid[0].length;
+
+        row = new int[]{0 , -1 , 1, 0};
+        col = new int[]{-1 ,0 , 0 , 1};
 
         int visit[][] = new int[n][m];
 
-        int row[] = {-1 , 1 , 0 , 0};
-        int col[] = {0, 0 , -1 ,1};
+        for(int v[] : visit){
+            Arrays.fill(v , -1);
+        }
 
         Queue<Pair> queue = new LinkedList<>();
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(arr[i][j] == 0){
-                    visit[i][j] = 1;
-                }
-                if((i == 0 || i == n-1 || j == 0 || j == m-1) &&  arr[i][j] == 1){
-                    visit[i][j] = 1;
-                    queue.add(new Pair(i , j));
+        for(int i = 0;i<n;i++){
+            for(int j  = 0 ; j < m;j++){
+                if((i == 0 || i == n-1 || j == 0 ||  j == m-1) && (visit[i][j] == -1 && grid[i][j] == 1)){
+                    dfs(grid , visit , i , j);
                 }
             }
         }
 
-        while(queue.size() > 0){
-            int r = queue.peek().row;
-            int c = queue.peek().col;
+        int res = 0;
 
-            queue.poll();
-
-            for(int i=0;i<4;i++){
-                int nr = r + row[i];
-                int nc = c + col[i];
-
-                if(nr >= 0 && nr < n && nc >=0 && nc < m && visit[nr][nc] == 0){
-                    visit[nr][nc] = 1;
-                    queue.add(new Pair(nr , nc));
-                }
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(visit[i][j] == -1 && grid[i][j] == 1) res++;
             }
         }
 
-        int cnt = 0;
+        return res;
+    }
+    public void dfs(int[][] grid , int[][] visit , int r , int c){
+        visit[r][c] = 1;
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(visit[i][j] == 0){
-                    cnt++;
-                }
+        for(int i = 0 ; i < 4;i++){
+            int nr = r  + row[i];
+            int nc = c + col[i];
+
+            if(nr >= 0 && nr < n && nc >= 0 && nc < m && visit[nr][nc] != 1 && grid[nr][nc] == 1){
+                dfs(grid , visit , nr , nc);
             }
         }
-
-        return cnt;
     }
 }
