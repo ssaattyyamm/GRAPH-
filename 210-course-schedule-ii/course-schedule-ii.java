06202889c8bd66cd439b51010c6d7aab22437d02
noobcoderhuuu <satyam.singh.931559@gmail.com>
2026-07-16 12:48:23 +0530

    Time: 5 ms (83.24%) | Memory: 47 MB (40.20%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,48 +1,40 @@
 class Solution {
-    public int[] findOrder(int V, int[][] prerequisites) {
-        /// easy prblm anyone can do this : 
-
+    public int[] findOrder(int V , int[][] arr) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 
-        for(int i=0;i<V;i++){
+        for(int i = 0; i < V;i++){
             adj.add(new ArrayList<>());
-        }
-
-        for(int e[] : prerequisites){
-            adj.get(e[1]).add(e[0]);
         }
 
         int indeg[] = new int[V];
 
-        for(int i=0;i<V;i++){
-            for(int x : adj.get(i)){
-                indeg[x]++;
-            }
+        for(int e[] :arr){
+            adj.get(e[1]).add(e[0]);
+            indeg[e[0]]++;
         }
 
         Queue<Integer> queue = new LinkedList<>();
-
         int res[] = new int[V];
-        int k=0;
+        int k = 0;
 
-        for(int i=0;i<V;i++){
-            if(indeg[i] == 0) queue.add(i);
-        }
-
-        // ArrayList<Integer> res = new ArrayList<>();
-
-        while(queue.size() > 0){
-            int curr = queue.peek();
-
-            queue.poll();
-
-            res[k++] = curr;
-
-            for(int x : adj.get(curr)){
-                indeg[x]--;
-                if(indeg[x] == 0) queue.add(x);
+        for(int i = 0 ; i < V;i++){
+            if(indeg[i] == 0){
+                queue.add(i);
             }
         }
+
+        while(queue.size() > 0){
+            int node = queue.poll();
+            res[k++] = node;
+
+            for(int i = 0 ; i < adj.get(node).size();i++){
+                int next = adj.get(node).get(i);
+                indeg[next]--;
+
+                if(indeg[next] == 0) queue.add(next);
+            }
+        }
+
         if(k == V) return res;
 
         else return new int[0];
