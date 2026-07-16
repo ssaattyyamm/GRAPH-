f32ca93d274dc5239570a222ebd8ecbe2fed1757
noobcoderhuuu <satyam.singh.931559@gmail.com>
2026-07-16 12:15:42 +0530

    Time: 26 ms (38.39%) | Memory: 62.4 MB (49.25%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -2,16 +2,16 @@ class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int n = graph.length;
 
-        ArrayList<ArrayList<Integer>>  adj = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for(int i = 0 ; i < n;i++){
+            adj.add(new ArrayList());
+        }
 
         int indeg[] = new int[n];
 
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
-        }
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<graph[i].length;j++){
+        for(int i = 0 ; i < n ;i++){
+            for(int j = 0 ; j < graph[i].length;j++){
                 adj.get(graph[i][j]).add(i);
                 indeg[i]++;
             }
@@ -19,34 +19,30 @@ class Solution {
 
         Queue<Integer> queue = new LinkedList<>();
 
-        for(int i=0;i<n;i++){
+        for(int i = 0 ; i < n;i++){
             if(indeg[i] == 0){
                 queue.add(i);
             }
         }
 
-        ArrayList<Integer> res = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
 
         while(queue.size() > 0){
-            int node = queue.peek();
+            int curr = queue.poll();
+            ans.add(curr);
 
-            queue.poll();
+            for(int i = 0 ;i < adj.get(curr).size();i++){
+                int next = adj.get(curr).get(i);
+                indeg[next]--;
 
-            res.add(node);
-
-            for(int i=0;i<adj.get(node).size();i++){
-                int curr = adj.get(node).get(i);
-
-                indeg[curr]--;
-
-                if(indeg[curr] == 0){
-                    queue.add(curr);
+                if(indeg[next] == 0){
+                    queue.add(next);
                 }
             }
         }
 
-        Collections.sort(res);
+        Collections.sort(ans);
 
-        return res;
+        return ans;
     }
 }
