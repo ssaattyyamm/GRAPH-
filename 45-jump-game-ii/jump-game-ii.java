694efb34a5b806156c8f43b3d2ea7c837f819e2b
noobcoderhuuu <satyam.singh.931559@gmail.com>
2026-09-12 17:07:15 +0530

    Time: 122 ms (5.71%) | Memory: 47.4 MB (21.24%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,38 +1,35 @@
 class Solution {
-    int inf = (int) 1e9;
-
+    int dp[];
     public int jump(int[] nums) {
         int n = nums.length;
 
-        int dp[] = new int[n];
+        dp = new int[n];
 
-        dp[n - 1] = 0;
+        Arrays.fill(dp , -1);
 
-        for (int i = n-2; i >=0; i--) {
-            int ans = inf / 2;
+        return helper(nums , 0);
+    }
+    public int helper(int[] nums , int idx){
+        if(idx >= nums.length -1){
+            return 0;
+        }
 
-            for (int j = i + 1 ; j <= Math.min(i + nums[i], n - 1); j++) {
-                ans = Math.min(ans, 1 + dp[j]);
+        if(dp[idx] != -1) return dp[idx];
+
+        if(nums[idx] == 0) return Integer.MAX_VALUE;
+
+        int ans = Integer.MAX_VALUE;
+
+        for(int i = 1 ; i <= nums[idx] ; i++){
+            int next = helper(nums , idx + i);
+
+            if(next != Integer.MAX_VALUE){
+                ans = Math.min(ans , next + 1);
             }
 
-            dp[i] = ans;
+            // ans = Math.min(ans , curr);
         }
 
-        return dp[0];
-    }
-
-    public int helper(int[] nums, int idx) {
-        if (idx == nums.length - 1)
-            return 0;
-
-        // if(nums[idx] == 0) return inf;
-
-        int ans = inf / 2;
-
-        for (int i = idx + 1; i <= Math.min(idx + nums[idx], nums.length - 1); i++) {
-            ans = Math.min(ans, 1 + helper(nums, i));
-        }
-
-        return ans;
+        return dp[idx] = ans;
     }
 }
