b70c8088c8d50b1fcc3e70464e68461ed3e83b07
noobcoderhuuu <satyam.singh.931559@gmail.com>
2026-09-12 09:40:57 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (99.78%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,25 +1,32 @@
 class Solution {
     public int rob(int[] nums) {
-        // this is the very simple problem 
         int n = nums.length;
 
-        if(n == 1) return nums[0];
+        int dp[] = new int[n + 1];
 
-        int dp[] = new int[n];
+        // state ::  dp[i] = max amnt of money that can i earn 
 
-        dp[0] = nums[0];
+        dp[n-1] = nums[n-1];
 
-        dp[1] = Math.max(nums[0] , nums[1]);
+        for(int i = n-2 ; i >=0 ; i--){
+            int pick = nums[i] + dp[i+2];
 
-        for(int i=2;i<n;i++){
-            dp[i] = Math.max(nums[i] + dp[i-2] , dp[i-1]);
+            int skip = dp[i + 1];
+
+            dp[i] = Math.max(pick , skip);
         }
 
-        return dp[n-1];
-    }
-    public int helper(int idx  , int[] nums ){
-        if(idx  >= nums.length) return 0;
+        return dp[0];
 
-        return Math.max(nums[idx] + helper(idx + 2 , nums) , helper(idx + 1 , nums));
+    }
+    public int helper(int[] nums , int idx){
+        if(idx >= nums.length) return 0;
+
+        int pick = nums[idx] + helper(nums , idx + 2);
+
+        int skip = helper(nums , idx + 1);
+
+        return Math.max(pick , skip);
+
     }
 }
