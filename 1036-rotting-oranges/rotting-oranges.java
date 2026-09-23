b832b8de09e546fddf6ff0af3fd85034463c79f6
noobcoderhuuu <satyam.singh.931559@gmail.com>
2026-09-23 13:58:57 +0530

    Time: 2 ms (87.48%) | Memory: 44.4 MB (13.69%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,70 +1,63 @@
-class Pair {
-    int first;
-    int second;
-    int tm;
-
-    public Pair(int first, int second, int tm) {
-        this.first = first;
-        this.second = second;
-        this.tm = tm;
-    }
-}
-
 class Solution {
+    class Tuple{
+        int row;
+        int col;
+        int min;
+
+        public Tuple(int row , int col , int min){
+            this.row = row;
+            this.col = col;
+            this.min = min;
+        }
+    }
     public int orangesRotting(int[][] grid) {
-        int n = grid.length; // row
-        int m = grid[0].length; // col
+        int  n = grid.length;
+        int m = grid[0].length;
 
-        Queue<Pair> q = new LinkedList<>();  /// queue with data type Pair
+        Queue<Tuple> queue = new LinkedList<>();
 
-        int visit[][] = new int[n][m];
-
-        int fresh = 0; // to count fresh oranges
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        for(int i = 0; i < n;i++){
+            for(int j = 0 ; j < m;j++){
                 if(grid[i][j] == 2){
-                    q.add(new Pair(i , j , 0));
-                    visit[i][j] = 2;
-                }
-                else{
-                    visit[i][j] = 0;
-                }
-
-                if(grid[i][j] == 1) fresh++;
-            }
-        }
-
-        int dir[][] = {{1,0},{0,1},{-1,0},{0,-1}};
-
-        int res = 0;
-
-        int rotten = 0;
-
-        // applying bfs 
-        while(q.size() > 0){
-            int r = q.peek().first;
-            int c = q.peek().second;
-            int time = q.peek().tm;
-
-            res = Math.max(res , time);
-
-            q.remove();
-
-            for(int d[] : dir){
-                int nr = r + d[0];
-                int nc = c + d[1];
-
-                if(nr >=0 && nr < n && nc >=0 && nc < m && visit[nr][nc] == 0 && grid[nr][nc] == 1){
-                    q.add(new Pair(nr , nc , time +1));
-                    visit[nr][nc] = 2;
-                    rotten++;
+                    queue.add(new Tuple(i , j , 0));
+                    grid[i][j] = 0;
                 }
             }
         }
 
-        if(rotten != fresh) return -1;
+        int ans = 0;
 
-        else return res; 
+        int r[] = {-1 , 1 , 0 , 0};
+        int c[] ={0 , 0 , -1 , 1};
+
+        while(queue.size() > 0){
+            Tuple t = queue.poll();
+            int row = t.row;
+            int col = t.col;
+            int min = t.min;
+
+            ans = Math.max(ans , min);
+
+
+            for(int i = 0 ; i < 4;i++){
+                int nr = row + r[i];
+                int nc = col + c[i];
+
+                if(nr >=0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1){
+                    grid[nr][nc] = 0;
+                    queue.add(new Tuple(nr , nc , min + 1));
+                }
+            }
+        }
+
+        for(int i = 0 ; i < n;i++){
+            for(int  j = 0 ; j < m;j++){
+                if(grid[i][j] == 1){
+                    return -1;
+                }
+            }
+        }
+
+        return ans;
     }
 }
